f4a0212f00a66fa3d9be1f282f7a13ea50ac4cf8
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-14 16:22:53 -0700

    add header and about section to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-// OBJECTIVES
-// All previous standards (last week's objectives) are in place (see below)
-// Site uses branching to return the result.
-// jQuery is used to show and hide the result after questions are answered and submitted. (Tip: test your app several times with different answers.)
-// Variable names are descriptive of what they represent.
-// Web page is styled using Bootstrap and custom CSS.
-
-// Question 1 card id=personalityQuestion form id=personality
-// Question 2 card id=interestQuestion form id=interest
-// Question 3 card id=colorQuestion form id=color
-// Question 4 card id=musicQuestion form id=music
-// Question 4 card id=animalQuestion form id=animal
-
 // User Interface
 $(document).ready(function() {
   console.log("Script executing!")
