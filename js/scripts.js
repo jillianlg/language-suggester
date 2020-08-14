076e6cb99c875f81fce6ee8374230157891b707e
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-14 10:46:17 -0700

    update index.html with form question 1 changed from radio to select boxes, add button, add language suggester answer cards with img

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// OBJECTIVES
+// All previous standards (last week's objectives) are in place (see below)
+// Site uses branching to return the result.
+// jQuery is used to show and hide the result after questions are answered and submitted. (Tip: test your app several times with different answers.)
+// Variable names are descriptive of what they represent.
+// Web page is styled using Bootstrap and custom CSS.
+
+// Question 1  id=personalityQuestion value=low-leve/ value=high-level
