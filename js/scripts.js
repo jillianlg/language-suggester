6a80e976c45dc6e29656bc31955f635cc7f82262
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-14 18:13:19 -0700

    move and about section to end

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,12 @@ $(document).ready(function() {
     $(".java-script, .python, .rudy").hide();
 
     if (personalityInput === "controled" && interestInput === "reading murder mystery" || personalityInput === "detail oriented" && interestInput === "playing Sudoku" || personalityInput === "controled" && interestInput === "playing Sudoku" || personalityInput === "detail oriented" && interestInput === "reading murder mystery") {
-      $(".python, .weSuggest").show(); 
+      $(".python, .weSuggest, .about").show(); 
     } else if (personalityInput === "spontaneous" && interestInput === "going to a concert" || personalityInput === "spontaneous" && interestInput === "playing video games" || personalityInput === "care free" && interestInput === "going to a concert" || personalityInput === "care free" && interestInput === "playing video games") {
-      $(".ruby, .weSuggest").show(); 
+      $(".ruby, .weSuggest, .about").show(); 
 
     } else {
-      $(".java-script, .weSuggest").show(); 
+      $(".java-script, .weSuggest, .about").show(); 
     } 
   });
 });
